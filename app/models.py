16dbcccc5dbb8f7de61b1c4dee13a2944c350055
omognuni <melbong0069@gmail.com>
2022-11-15 18:13:17 +0900

    [Feat] Audio, Text, Project 모델 생성

--- app/models.py
+++ app/models.py
@@ -1,7 +1,38 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, ForeignKey, String, Integer, Text, DateTime
+from sqlalchemy.orm import relationship
+from datetime import datetime
 from db import Base
+
 
 class Audio(Base):
     __tablename__ = 'audio'
     
+    id = Column(Integer, primary_key=True)
+    updated_at = Column(DateTime, default=datetime.now())
+    speed = Column(Integer, default=1)
+    project_id = Column(Integer, ForeignKey("project.id"))
     
+    texts = relationship("AudioText", back_populates="audio")
+    project = relationship("Project", back_populates="audios")
+
+    
+class Project(Base):
+    __tablename__ = 'project'
+    
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    created_at = Column(DateTime, default=datetime.now())
+    updated_at = Column(DateTime, default=datetime.now())
+    
+    audios = relationship("Audio", back_populates="project")
+    
+    
+class AudioText(Base):
+    __tablename__ = 'audio_text'
+    
+    id = Column(Integer, primary_key=True)
+    content = Column(Text)
+    index = Column(Integer)
+    audio_id = Column(Integer, ForeignKey("audio.id"))
+    
+    audio = relationship("Audio", back_populates="texts")
